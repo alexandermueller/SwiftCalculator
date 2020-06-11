bed45c3301ded5020e2df6b364f6689fe941176a
Alex Mueller <alexander.georg.mueller@gmail.com>
2020-06-11 02:25:36 -0400

    Fix Parenthesis Implementation, Complete Some More Unit Tests.

--- Calculator/CalculatorTests/ArithmeticExpressionTests.swift
+++ Calculator/CalculatorTests/ArithmeticExpressionTests.swift
@@ -27,29 +27,30 @@ class ArithmeticExpressionTests: XCTestCase {
                 UnitTest([], .empty)
             ]),
             
-            "Number, Double" : (.equivalent, [
+            "Number, Decimal" : (.equivalent, [
                 UnitTest(["0."], .error),
                 UnitTest(["1"], .number(1)),
-                UnitTest(["0.000001"], .number(0.000001))
+                UnitTest(["0.000001"], .number(0.000001)),
+                UnitTest(["10.000001"], .number(10.000001)),
             ]),
             
             "Addition" : (.equivalent, [
                 UnitTest(["0", "+"], .error),
                 UnitTest(["0", "+", "0."], .error),
                 UnitTest(["1", "+", "2"], .addition(.number(1), .number(2))),
-                UnitTest(["1", "+", "2", "+", "1", "+", "2"], .addition(.addition(.addition(.number(1), .number(2)), .number(1)), .number(2)))
+                UnitTest(["1", "+", "2", "+", "1", "+", "2"], .addition(.addition(.addition(.number(1), .number(2)), .number(1)), .number(2))),
             ]),
             
             "Subtraction" : (.equivalent, [
                 UnitTest(["0", "-"], .error),
                 UnitTest(["0", "-", "0."], .error),
                 UnitTest(["1", "–", "1"], .subtraction(.number(1), .number(1))),
-                UnitTest(["1", "+", "2", "+", "1", "+", "2"], .addition(.addition(.addition(.number(1), .number(2)), .number(1)), .number(2)))
+                UnitTest(["1", "+", "2", "+", "1", "+", "2"], .addition(.addition(.addition(.number(1), .number(2)), .number(1)), .number(2))),
             ]),
             
-            "Addition + Subtraction" : (.equivalent, [
+            "Addition & Subtraction" : (.equivalent, [
                 UnitTest(["1", "+", "1", "–", "1"], .subtraction(.addition(.number(1), .number(1)), .number(1))),
-                UnitTest(["1", "–", "1", "+", "1"], .addition(.subtraction(.number(1), .number(1)), .number(1)))
+                UnitTest(["1", "–", "1", "+", "1"], .addition(.subtraction(.number(1), .number(1)), .number(1))),
             ]),
             
             "Modulo" : (.equivalent, [
@@ -57,7 +58,7 @@ class ArithmeticExpressionTests: XCTestCase {
                 UnitTest(["0", "%", "0."], .error),
                 UnitTest(["1", "+", "1", "%", "1"], .addition(.number(1), .modulo(.number(1), .number(1)))),
                 UnitTest(["1", "%", "1", "–", "1"], .subtraction(.modulo(.number(1), .number(1)), .number(1))),
-                UnitTest(["3", "%", "4", "%", "5"], .modulo(.modulo(.number(3), .number(4)), .number(5)))
+                UnitTest(["3", "%", "4", "%", "5"], .modulo(.modulo(.number(3), .number(4)), .number(5))),
             ]),
             
             "Negation" : (.equivalent, [
@@ -67,6 +68,38 @@ class ArithmeticExpressionTests: XCTestCase {
                 UnitTest(["-", "1", "–", "1"], .subtraction(.negation(.number(1)), .number(1))),
             ]),
             
+            "Multiplication" : (.equivalent, [
+                UnitTest(["1", "x", "2"], .multiplication(.number(1), .number(2))),
+                UnitTest(["1", "x", "2", "x", "3"], .multiplication(.multiplication(.number(1), .number(2)), .number(3))),
+                UnitTest(["-", "1", "x", "2"], .multiplication(.negation(.number(1)), .number(2))),
+                UnitTest(["3", "x", "-", "1"], .multiplication(.number(3), .negation(.number(1)))),
+            ]),
+            
+            "Division" : (.equivalent, [
+                UnitTest(["1", "÷", "2"], .division(.number(1), .number(2))),
+                UnitTest(["1", "÷", "2", "÷", "3"], .division(.division(.number(1), .number(2)), .number(3))),
+                UnitTest(["-", "1", "÷", "2"], .division(.negation(.number(1)), .number(2))),
+                UnitTest(["3", "÷", "-", "1"], .division(.number(3), .negation(.number(1)))),
+            ]),
+            
+            "Multiplication & Division & Negation" : (.equivalent, [
+                UnitTest(["1", "x", "2", "÷", "3"], .division(.multiplication(.number(1), .number(2)), .number(3))),
+                UnitTest(["1", "÷", "2", "x", "3"], .multiplication(.division(.number(1), .number(2)), .number(3))),
+                UnitTest(["1", "÷", "3", "x",  "1", "÷", "2", "x", "3"], .multiplication(.division(.multiplication(.division(.number(1), .number(3)), .number(1)), .number(2)), .number(3))),
+                UnitTest(["1", "x", "3", "÷",  "2", "x", "1", "÷", "2"], .division(.multiplication(.division(.multiplication(.number(1), .number(3)), .number(2)), .number(1)), .number(2))),
+                UnitTest(["1", "x", "-", "3", "÷",  "2", "x", "-", "1", "÷", "2"], .division(.multiplication(.division(.multiplication(.number(1), .negation(.number(3))), .number(2)), .negation(.number(1))), .number(2))),
+                UnitTest(["1", "+", "1", "x", "3"], .addition(.number(1), .multiplication(.number(1), .number(3)))),
+                UnitTest(["1", "÷", "1", "–", "3"], .subtraction(.division(.number(1), .number(1)), .number(3))),
+                UnitTest(["9", "÷", "-", "3", "–", "3", "x", "3", "+", "1"], .addition(.subtraction(.division(.number(9), .negation(.number(3))), .multiplication(.number(3), .number(3))), .number(1))),
+                UnitTest(["1", "%", "2", "÷", "1", "–", "3", "%", "5"], .subtraction(.modulo(.number(1), .division(.number(2), .number(1))), .modulo(.number(3), .number(5)))),
+            ]),
+            
+            // abs, sum
+            // factorial
+            // exponent
+            // root
+            // sqrt, inv, square
+            
             //            UnitTest(["(", "1", ")"], .number(1)),
             //            UnitTest(["(", "1", ")", "+", "(", "1", ")"], .addition(.number(1), .number(1))),
             //            UnitTest(["(", "1", "–", "(", "2", "+", "1", ")", ")"], .subtraction(.number(1), .addition(.number(2), .number(1)))),
@@ -74,11 +107,11 @@ class ArithmeticExpressionTests: XCTestCase {
             //            UnitTest(["1", "÷", "3", "^", "(", "5", "–", "7", ")"], .division(.number(1), .exponentiation(.number(3), .subtraction(.number(5), .number(7))))),
             //            UnitTest(["-", "3", "^", "2", "^", "0.5"], .negation(.exponentiation(.number(3), .exponentiation(.number(2), .number(0.5))))),
             //            UnitTest(["(", "-3", "^", "2", ")", "^", "0.5"], .exponentiation(.exponentiation(.number(-3), .number(2)), .number(0.5))),
-        //            UnitTest(["1", "+", "2", "^", "3", "–", "5", "^", "2", "÷", "3", "÷", "3"], .subtraction(.addition(.number(1.0), .exponentiation(.number(2.0), .number(3.0))), .division(.division(.exponentiation(.number(5.0), .number(2.0)), .number(3.0)), .number(3.0)))),
-        //            UnitTest(["1", "*√", "2"], .root(.number(1), .number(2))),
-        //            UnitTest(["2", "^", "2", "^", "0.5"], .exponentiation(.number(2), .exponentiation(.number(2), .number(0.5)))),
-        //            UnitTest(["2", "^", "-", "2", "^", "0.5"], .exponentiation(.number(2), .negation(.exponentiation(.number(2), .number(0.5))))),
-        //            UnitTest(["2", "*√", "2", "^", "2"], .root(.number(2), .exponentiation(.number(2), .number(2)))),
+            //            UnitTest(["1", "+", "2", "^", "3", "–", "5", "^", "2", "÷", "3", "÷", "3"], .subtraction(.addition(.number(1.0), .exponentiation(.number(2.0), .number(3.0))), .division(.division(.exponentiation(.number(5.0), .number(2.0)), .number(3.0)), .number(3.0)))),
+            //            UnitTest(["1", "*√", "2"], .root(.number(1), .number(2))),
+            //            UnitTest(["2", "^", "2", "^", "0.5"], .exponentiation(.number(2), .exponentiation(.number(2), .number(0.5)))),
+            //            UnitTest(["2", "^", "-", "2", "^", "0.5"], .exponentiation(.number(2), .negation(.exponentiation(.number(2), .number(0.5))))),
+            //            UnitTest(["2", "*√", "2", "^", "2"], .root(.number(2), .exponentiation(.number(2), .number(2)))),
             // TODO: Add more unit tests
         ]
         
@@ -460,12 +493,6 @@ class ArithmeticExpressionTests: XCTestCase {
                 UnitTest(.summation(.number(3)), 6),
                 UnitTest(.summation(.number(.infinity)), .infinity),
             ]),
-            
-            /** Rank (from most important to least):
-             *  -1. Parenthesis
-             **/
-            
-            // TODO: Complete function unit tests
         ]
         
         evaluateTestCaseSuite(testCaseSuite, using: { input in
@@ -489,24 +516,3 @@ class ArithmeticExpressionTests: XCTestCase {
         }
     }
 }
-
-
-
-//            UnitTest(.multiplication(.number(1), .error), .nan),
-//            UnitTest(.multiplication(.error, .number(1)), .nan),
-//            UnitTest(.multiplication(.number(1), .number(2)), 2),
-//            UnitTest(.multiplication(.number(2), .negation(.number(3))), -6),
-//            UnitTest(.multiplication(.addition(.number(2), .number(4)), .subtraction(.number(2), .number(5))), -18),
-//            UnitTest(.division(.number(1), .error), .nan),
-//            UnitTest(.division(.error, .number(1)), .nan),
-//            UnitTest(.division(.number(1), .number(0)), .infinity),
-//            UnitTest(.division(.number(1), .number(.infinity)), 0),
-//            UnitTest(.division(.number(.infinity), .number(.infinity)), -.nan),
-//            UnitTest(.division(.number(0), .number(.infinity)), 0),
-//            UnitTest(.division(.number(1), .number(2)), 0.5),
-//            UnitTest(.exponentiation(.error, .number(2)), .nan),
-//            UnitTest(.exponentiation(.number(1), .error), .nan),
-//            UnitTest(.exponentiation(.number(4), .number(0.5)), 2),
-//            UnitTest(.exponentiation(.number(3), .number(0)), 1),
-//            UnitTest(.exponentiation(.number(.infinity), .number(0)), 1),
-//            UnitTest(.exponentiation(.number(0), .number(.infinity)), 0)
